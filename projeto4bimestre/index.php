6ab5e80b2697beb7f085ecc3418b4aa6a9e794b8
lucas41 <delfini.lucas@hotmail.com>
2017-10-08 10:21:23 -0300

    vamo ve se vai

---
+++ projeto4bimestre/index.php
@@ -0,0 +1,185 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        
+         <!--Import Google Icon Font-->
+         <link href="css/materialize.min.css" rel="stylesheet" type="text/css"/>
+         <link href="css/estilo.css" rel="stylesheet" type="text/css"/>
+        
+           
+
+    </head>
+    <body  class="grey  grey darken-4" >
+        
+  <nav>
+    <div class="nav-wrapper brown darken-3 ">
+           
+      <a href="#" class="brand-logo right">Logo </a>
+   
+      <ul id="nav-mobile" class="left hide-on-med-and-down"> 
+         
+        <li><a href="index.php">Home</a></li>
+        <li><a href="componentes.html">Componentes   </a></li>
+        <li><a href="faleconosco.html">fale conosco</a></li>
+      </ul>
+    </div>
+  </nav>  
+        
+       
+        
+
+        
+        
+        
+        <br>
+        
+      <div class="row">
+        <div class="col s4 m4">
+          <div class="card">
+            <div class="card-image red darken-4">
+              
+              <img src="video/717208.jpg" alt=""/>
+              <span class="card-title">Dark souls 2</span>
+            </div>
+            <div class="card-content">
+              <p>Dark Souls II é o terceiro jogo de ação e role-playing da saga Souls, foi desenvolvido pela FromSoftware e publicado pelos Bandai Namco Games. Ele foi lançado para o 
+                  Microsoft Windows, PlayStation 3 e Xbox 360. A série é famosa por sua alta dificuldade, que a faz receber muitos elogios e críticas..</p>
+              <br>
+                <p>O jogo começa de forma semelhante aos outros RPGs: o jogador cria seu personagem do zero,
+                    podendo personalizar diversas características do mesmo, como o tipo físico, cor de pele e estilo de combate. </p>
+                <br><br><br>
+            
+            </div>
+            <div class="card-action">
+              <a href="darksouls.html">Veja a materia completa aqui</a>
+              
+            </div>
+          </div>
+        </div>
+        
+          
+         
+        
+        <div class="col s4 m8">
+          <div class="card">
+            <div class="card-image red darken-4">
+              <img src="video/rainbow_six_siege_operation_blood_orchid_hd_4k_2017-wide.jpg" width="700" height="470">
+              <span class="card-title">Rainbow six siege</span>
+            </div>
+            <div class="card-content">
+              <p>Em Operação Blood Orchid, dois Agentes da Unidade de Deveres Especiais juntam-se à Equipe Rainbow para invadir o Parque Temático. Após a otimização dos nossos dados, essa terceira temporada vai apresentar a reconstrução de Agentes e mapas, como o retrabalho das texturas,
+                  iluminação dinâmica e estática polida, domo de céu mais rico, novas personalizações e muito mais.</p>
+            </div>
+            <div class="card-action">
+              <a href="rainbowsixsiege.html">Veja a materia completa aqui</a>
+            </div>
+          </div>
+        </div>
+      </div>    
+        
+        
+        <div class="row">
+           <div class="container col m8">
+               
+            
+            
+         
+ <div class="video">
+      <video width="850" height="420" controls>
+          <source  src="video/The Witcher 2 - intro Cinematic in 1080p.mp4" >
+      </video>
+      </div>
+           </div>
+          
+           
+    
+  
+ <div class="row">
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="video/TheWitcher2_Medallion_1920x1200.jpg" width="150" height="235">
+              <span class="card-title">The witcher 2</span>
+            </div>
+            <div class="card-content">
+              <p>The Witcher 2: Assassins of Kings é um jogo eletrônico dos gêneros ação e RPG desenvolvido pelo estúdio CD Projekt RED para as plataformas Microsoft Windows, 
+                  Xbox 360, OS X, Linux e SteamOS, sendo a sequência direta de The Witcher, lançado em 2007</p>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+            
+            <br>
+            
+      <div class="row">
+        <div class="col s12 m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="video/portal2-1.jpg" width="150" height="235">
+              <span class="card-title">Portal 2</span>
+            </div>
+            <div class="card-content">
+              <p>Portal 2 é um jogo de ação/puzzle em primeira pessoa desenvolvido e publicado pela Valve Corporation. O título é uma sequência 
+                  de Portal e foi lançado no dia 19 de abril de 2011 para Microsoft Windows, OS X, Linux, PlayStation 3 e Xbox 360.</p>
+            </div>
+           
+          </div>
+        </div>
+ 
+            
+  <div class="video">
+       <video width="815" height="395" controls>
+         <source  src="video/Portal 2 Teaser Trailer.mp4" type="video/mp4">
+       </video>
+      </div>
+           </div>
+  
+            
+          
+
+     
+  
+  
+
+
+
+    <footer class="page-footer brown darken-3">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contate-nos</h5>
+                <p class="grey-text text-lighten-4">Nossas redes sociais fale conosco e de seu feedback
+                <3</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                 <h5 class="white-text">Facebook</h5>  <img src="video/facebook_logos_PNG19751.png" width="40" height="30"/> &nbsp
+                 <img src="video/twitter-2012-negative-logo-5C6C1F1521-seeklogo.com.png" width="30" height="30"/>
+                    <ul>
+                    
+                  <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/lucas.delfini.16" >Lucas Delfini</a></li> 
+                  <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/graziela.elisandra">Graziela elisandra</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/bruna.fernanda.946">Bruna fernanda</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://www.facebook.com/isabella.menato">isabella menato</a></li>
+                </ul>
+           
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+                © 2017 Copyright by Lucas, Bruna, Grazy e isa <br>
+               
+            </div>
+          </div>
+        
+        
+        <script src="js/materialize.js" type="text/javascript"></script>
+        <script src="js/materialize.min.js" type="text/javascript"></script>
+
+    </footer>
+</body>
+</html>
+        
